scylladb-zpp-2024-javascript-driver/scylladb-javascript-driver

Add simple QueryOptionsWrapper
Currently this objects keeps only some of the parameters exposed in the API.
Remaining parameters will need to be added at a later time.

(All skipped options were due to complexity of the expected type)

src/requests/request.rs
@@ -1,3 +1,4 @@
+use napi::bindgen_prelude::BigInt;
 use scylla::prepared_statement::PreparedStatement;
 
 use crate::result::{map_column_type_to_cql_type, CqlType};
@@ -5,6 +6,32 @@ use crate::result::{map_column_type_to_cql_type, CqlType};
 #[napi]
 pub struct PreparedStatementWrapper {
     pub(crate) prepared: PreparedStatement,
+}
+
+#[napi]
+pub struct QueryOptionsWrapper {
+    pub auto_page: Option<bool>,
+    pub capture_stack_trace: Option<bool>,
+    pub consistency: Option<u16>,
+    pub counter: Option<bool>,
+    // customPayload?: any;
+    // executionProfile?: string | ExecutionProfile;
+    pub fetch_size: Option<i32>,
+    // hints?: string[] | string[][];
+    // host?: Host;
+    pub is_idempotent: Option<bool>,
+    pub keyspace: Option<String>,
+    pub logged: Option<bool>,
+    // pageState?: Buffer | string;
+    pub prepare: Option<bool>,
+    pub read_timeout: Option<i32>,
+    // retry?: policies.retry.RetryPolicy;
+    pub routing_indexes: Option<Vec<i32>>,
+    // routingKey?: Buffer | Buffer[];
+    pub routing_names: Option<Vec<String>>,
+    pub serial_consistency: Option<i32>,
+    pub timestamp: Option<BigInt>,
+    pub trace_query: Option<bool>,
 }
 
 #[napi]
@@ -16,5 +43,29 @@ impl PreparedStatementWrapper {
             .iter()
             .map(|e| map_column_type_to_cql_type(e.typ()))
             .collect()
+    }
+}
+
+#[napi]
+impl QueryOptionsWrapper {
+    #[napi]
+    pub fn empty_options() -> QueryOptionsWrapper {
+        QueryOptionsWrapper {
+            auto_page: None,
+            capture_stack_trace: None,
+            consistency: None,
+            counter: None,
+            fetch_size: None,
+            is_idempotent: None,
+            keyspace: None,
+            logged: None,
+            prepare: None,
+            read_timeout: None,
+            routing_indexes: None,
+            routing_names: None,
+            serial_consistency: None,
+            timestamp: None,
+            trace_query: None,
+        }
     }
 }

adespawn/nodejs

src/requests/request.rs
@@ -1,3 +1,4 @@
+use napi::bindgen_prelude::BigInt;
 use scylla::prepared_statement::PreparedStatement;
 
 use crate::{result::map_column_type_to_complex_type, types::type_wrappers::ComplexType};
@@ -5,6 +6,32 @@ use crate::{result::map_column_type_to_complex_type, types::type_wrappers::Compl
 #[napi]
 pub struct PreparedStatementWrapper {
     pub(crate) prepared: PreparedStatement,
+}
+
+#[napi]
+pub struct QueryOptionsWrapper {
+    pub auto_page: Option<bool>,
+    pub capture_stack_trace: Option<bool>,
+    pub consistency: Option<u16>,
+    pub counter: Option<bool>,
+    // customPayload?: any;
+    // executionProfile?: string | ExecutionProfile;
+    pub fetch_size: Option<i32>,
+    // hints?: string[] | string[][];
+    // host?: Host;
+    pub is_idempotent: Option<bool>,
+    pub keyspace: Option<String>,
+    pub logged: Option<bool>,
+    // pageState?: Buffer | string;
+    pub prepare: Option<bool>,
+    pub read_timeout: Option<i32>,
+    // retry?: policies.retry.RetryPolicy;
+    pub routing_indexes: Option<Vec<i32>>,
+    // routingKey?: Buffer | Buffer[];
+    pub routing_names: Option<Vec<String>>,
+    pub serial_consistency: Option<i16>,
+    pub timestamp: Option<BigInt>,
+    pub trace_query: Option<bool>,
 }
 
 #[napi]
@@ -17,5 +44,29 @@ impl PreparedStatementWrapper {
             .iter()
             .map(|e| map_column_type_to_complex_type(e.typ()))
             .collect()
+    }
+}
+
+#[napi]
+impl QueryOptionsWrapper {
+    #[napi]
+    pub fn empty_options() -> QueryOptionsWrapper {
+        QueryOptionsWrapper {
+            auto_page: None,
+            capture_stack_trace: None,
+            consistency: None,
+            counter: None,
+            fetch_size: None,
+            is_idempotent: None,
+            keyspace: None,
+            logged: None,
+            prepare: None,
+            read_timeout: None,
+            routing_indexes: None,
+            routing_names: None,
+            serial_consistency: None,
+            timestamp: None,
+            trace_query: None,
+        }
     }
 }