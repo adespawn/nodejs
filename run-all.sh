63976e7fc61fcc9180a1b9c89f8cf0527c0967cb
Stanisław Czech <stasio.czech@gmail.com>
2025-03-21 12:10:39 +0100

    Debug mode

--- run-all.sh
+++ run-all.sh
@@ -1,31 +1,41 @@
 git checkout -B CI-0
-git rebase second-iteration
+git rebase integration-hell
+git push --set-upstream integration-hell CI-0
 git push --force-with-lease
 git checkout -B CI-1
-git rebase second-iteration
+git rebase integration-hell
+git push --set-upstream integration-hell CI-1
 git push --force-with-lease
 git checkout -B CI-2
-git rebase second-iteration
+git rebase integration-hell
+git push --set-upstream integration-hell CI-2
 git push --force-with-lease
 git checkout -B CI-3
-git rebase second-iteration
+git rebase integration-hell
+git push --set-upstream integration-hell CI-3
 git push --force-with-lease
 git checkout -B CI-4
-git rebase second-iteration
+git rebase integration-hell
+git push --set-upstream integration-hell CI-4
 git push --force-with-lease
 git checkout -B CI-5
-git rebase second-iteration
+git rebase integration-hell
+git push --set-upstream integration-hell CI-5
 git push --force-with-lease
 git checkout -B CI-6
-git rebase second-iteration
+git rebase integration-hell
+git push --set-upstream integration-hell CI-6
 git push --force-with-lease
 git checkout -B CI-7
-git rebase second-iteration
+git rebase integration-hell
+git push --set-upstream integration-hell CI-7
 git push --force-with-lease
 git checkout -B CI-8
-git rebase second-iteration
+git rebase integration-hell
+git push --set-upstream integration-hell CI-8
 git push --force-with-lease
 git checkout -B CI-9
-git rebase second-iteration
+git rebase integration-hell
+git push --set-upstream integration-hell CI-9
 git push --force-with-lease
-git checkout second-iteration
+git checkout integration-hell
