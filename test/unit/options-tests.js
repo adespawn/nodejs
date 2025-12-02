042b40c9e84e0be9d9e878d29ffb70b500ef9c68
Stanisław Czech <stanislaw.czech@scylladb.com>
2025-12-02 11:35:39 +0100

    Add unit tests for client options
    Those unit tests check if the client options are correctly
    parsed from JavaScript objects to Rust structs, ensuring
    proper implementation of setRustOptions and SessionOptions struct definition.
    
    For the purpose of these tests, the define_js_to_rust_convertible_object
    struct now derives PartialEq and Eq traits.

---
+++ test/unit/options-tests.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const rust = require("../../index");
+const { setRustOptions } = require("../../lib/client-options");
+
+describe("Client options", function () {
+    it("should correctly convert client options", function () {
+        let options = {
+            contactPoints: ["Contact point 1", "Contact point 2"],
+            keyspace: "keyspace name",
+            applicationName: "App name",
+            applicationVersion: "App version",
+            id: "Client id",
+            maxPrepared: 2137,
+            credentials: {
+                username: "Unique username",
+                password: "Unique password",
+            },
+            sslOptions: { rejectUnauthorized: false },
+        };
+        rust.testsCheckClientOption(setRustOptions(options), 1);
+    });
+    it("should correctly convert empty client options", function () {
+        let options = {};
+        rust.testsCheckClientOption(setRustOptions(options), 2);
+    });
+});
