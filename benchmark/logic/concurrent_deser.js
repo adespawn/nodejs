scylladb-zpp-2024-javascript-driver/scylladb-javascript-driver

Compress SerDes benchmarks

benchmark/logic/concurrent_deser.js
@@ -2,45 +2,28 @@
 const async = require("async");
 // Possible values of argv[2] (driver) are scylladb-javascript-driver and cassandra-driver.
 const cassandra = require(process.argv[2]);
-const { getClientArgs, insertConcurrentDeSer } = require("./utils");
+const utils = require("./utils");
 const { exit } = require("process");
 
-const client = new cassandra.Client(getClientArgs());
-const iterCnt = parseInt(process.argv[3]); 
+const client = new cassandra.Client(utils.getClientArgs());
+const iterCnt = parseInt(process.argv[3]);
 
 async.series(
     [
-        function connect(next) {
-            client.connect(next);
-        },
-        function createKeyspace(next) {
-            // Keep replication one to reduce time spent in the database
-            const query =
-                "CREATE KEYSPACE IF NOT EXISTS benchmarks WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': '1' }";
-            client.execute(query, next);
-        },
-        // Drop table to ensure no overhead comes from having already some data in the database
-        function dropTable(next) {
-            const query =
-                "DROP TABLE IF EXISTS benchmarks.basic";
-            client.execute(query, next);
-        },
-        function createTable(next) {
-            const query =
-                "CREATE TABLE benchmarks.basic (id uuid, val int, tuuid timeuuid, ip inet, date date, time time, PRIMARY KEY(id))";
-            client.execute(query, next);
+        function initialize(next) {
+            utils.prepareDatabase(client, utils.tableSchemaDesSer, next);
         },
         async function insert(next) {
-            let allParameters = insertConcurrentDeSer(cassandra, iterCnt);
+            let allParameters = utils.insertConcurrentDeSer(cassandra, iterCnt);
             try {
-                const _result = await cassandra.concurrent.executeConcurrent(client, allParameters, {prepare: true});
+                const _result = await cassandra.concurrent.executeConcurrent(client, allParameters, { prepare: true });
             } catch (err) {
                 return next(err);
             }
             next();
         },
         async function select(next) {
-            
+
             let allParameters = [];
             for (let i = 0; i < iterCnt; i++) {
                 allParameters.push({
@@ -48,7 +31,7 @@ async.series(
                 });
             }
             try {
-                const _result = await cassandra.concurrent.executeConcurrent(client, allParameters, {prepare: true, collectResults: true});
+                const _result = await cassandra.concurrent.executeConcurrent(client, allParameters, { prepare: true, collectResults: true });
             } catch (err) {
                 return next(err);
             }

benchmark/logic/deser.js
@@ -2,40 +2,21 @@
 const async = require("async");
 // Possible values of argv[2] (driver) are scylladb-javascript-driver and cassandra-driver.
 const cassandra = require(process.argv[2]);
-const { getClientArgs, insertDeSer } = require("./utils");
+const utils = require("./utils");
 const { exit } = require("process");
 
-const client = new cassandra.Client(getClientArgs());
+const client = new cassandra.Client(utils.getClientArgs());
 const iterCount = parseInt(process.argv[3]);
 
 async.series(
     [
-        function connect(next) {
-            client.connect(next);
-        },
-        function createKeyspace(next) {
-            // Keep replication one to reduce time spent in the database
-            const query =
-                "CREATE KEYSPACE IF NOT EXISTS benchmarks WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': '1' }";
-            client.execute(query, next);
-        },
-        // Drop table to ensure no overhead comes from having already some data in the database
-        function dropTable(next) {
-            const query =
-                "DROP TABLE IF EXISTS benchmarks.basic";
-            client.execute(query, next);
-        },
-        function createTable(next) {
-            const query =
-                "CREATE TABLE benchmarks.basic (id uuid, val int, tuuid timeuuid, ip inet, date date, time time, PRIMARY KEY(id))";
-            client.execute(query, next);
+        function initialize(next) {
+            utils.prepareDatabase(client, utils.tableSchemaDesSer, next);
         },
         async function insert(next) {
-            const query =
-                "INSERT INTO benchmarks.basic (id, val, tuuid, ip, date, time) VALUES (?, ?, ?, ?, ?, ?)";
             for (let i = 0; i < iterCount; i++) {
                 try {
-                    await client.execute(query, insertDeSer(cassandra), {prepare: true});
+                    await client.execute(utils.DesSerInsertStatement, utils.insertDeSer(cassandra), { prepare: true });
                 } catch (err) {
                     return next(err);
                 }
@@ -53,7 +34,7 @@ async.series(
             }
             next();
         },
-        function r(){
+        function r() {
             exit(0);
         }
     ],
@@ -62,7 +43,7 @@ async.series(
             console.error("There was an error", err.message, err.stack);
             exit(1);
         }
-        
+
     },
 );
 

benchmark/logic/ser.js
@@ -2,47 +2,28 @@
 const async = require("async");
 // Possible values of argv[2] (driver) are scylladb-javascript-driver and cassandra-driver.
 const cassandra = require(process.argv[2]);
-const { getClientArgs, insertDeSer } = require("./utils");
+const utils = require("./utils");
 const { exit } = require("process");
 
-const client = new cassandra.Client(getClientArgs());
+const client = new cassandra.Client(utils.getClientArgs());
 const iterCount = parseInt(process.argv[3]);
 
 async.series(
     [
-        function connect(next) {
-            client.connect(next);
-        },
-        function createKeyspace(next) {
-            // Keep replication one to reduce time spent in the database
-            const query =
-                "CREATE KEYSPACE IF NOT EXISTS benchmarks WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': '1' }";
-            client.execute(query, next);
-        },
-        // Drop table to ensure no overhead comes from having already some data in the database
-        function dropTable(next) {
-            const query =
-                "DROP TABLE IF EXISTS benchmarks.basic";
-            client.execute(query, next);
-        },
-        function createTable(next) {
-            const query =
-                "CREATE TABLE benchmarks.basic (id uuid, val int, tuuid timeuuid, ip inet, date date, time time, PRIMARY KEY(id))";
-            client.execute(query, next);
+        function initialize(next) {
+            utils.prepareDatabase(client, utils.tableSchemaDesSer, next);
         },
         async function insert(next) {
-            const query =
-                "INSERT INTO benchmarks.basic (id, val, tuuid, ip, date, time) VALUES (?, ?, ?, ?, ?, ?)";
             for (let i = 0; i < iterCount * iterCount; i++) {
                 try {
-                    await client.execute(query, insertDeSer(cassandra), {prepare: true});
+                    await client.execute(utils.DesSerInsertStatement, utils.insertDeSer(cassandra), { prepare: true });
                 } catch (err) {
                     return next(err);
                 }
             }
             next();
         },
-        function r(){
+        function r() {
             exit(0);
         }
     ],
@@ -51,7 +32,7 @@ async.series(
             console.error("There was an error", err.message, err.stack);
             exit(1);
         }
-        
+
     },
 );
 

adespawn/nodejs

benchmark/logic/concurrent_deser.js
@@ -2,45 +2,28 @@
 const async = require("async");
 // Possible values of argv[2] (driver) are scylladb-javascript-driver and cassandra-driver.
 const cassandra = require(process.argv[2]);
-const { getClientArgs, insertConcurrentDeSer } = require("./utils");
+const utils = require("./utils");
 const { exit } = require("process");
 
-const client = new cassandra.Client(getClientArgs());
-const iterCnt = parseInt(process.argv[3]); 
+const client = new cassandra.Client(utils.getClientArgs());
+const iterCnt = parseInt(process.argv[3]);
 
 async.series(
     [
-        function connect(next) {
-            client.connect(next);
-        },
-        function createKeyspace(next) {
-            // Keep replication one to reduce time spent in the database
-            const query =
-                "CREATE KEYSPACE IF NOT EXISTS benchmarks WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': '1' }";
-            client.execute(query, next);
-        },
-        // Drop table to ensure no overhead comes from having already some data in the database
-        function dropTable(next) {
-            const query =
-                "DROP TABLE IF EXISTS benchmarks.basic";
-            client.execute(query, next);
-        },
-        function createTable(next) {
-            const query =
-                "CREATE TABLE benchmarks.basic (id uuid, val int, tuuid timeuuid, ip inet, date date, time time, PRIMARY KEY(id))";
-            client.execute(query, next);
+        function initialize(next) {
+            utils.prepareDatabase(client, utils.tableSchemaDeSer, next);
         },
         async function insert(next) {
-            let allParameters = insertConcurrentDeSer(cassandra, iterCnt);
+            let allParameters = utils.insertConcurrentDeSer(cassandra, iterCnt);
             try {
-                const _result = await cassandra.concurrent.executeConcurrent(client, allParameters, {prepare: true});
+                const _result = await cassandra.concurrent.executeConcurrent(client, allParameters, { prepare: true });
             } catch (err) {
                 return next(err);
             }
             next();
         },
         async function select(next) {
-            
+
             let allParameters = [];
             for (let i = 0; i < iterCnt; i++) {
                 allParameters.push({
@@ -48,7 +31,7 @@ async.series(
                 });
             }
             try {
-                const _result = await cassandra.concurrent.executeConcurrent(client, allParameters, {prepare: true, collectResults: true});
+                const _result = await cassandra.concurrent.executeConcurrent(client, allParameters, { prepare: true, collectResults: true });
             } catch (err) {
                 return next(err);
             }

benchmark/logic/deser.js
@@ -2,40 +2,21 @@
 const async = require("async");
 // Possible values of argv[2] (driver) are scylladb-javascript-driver and cassandra-driver.
 const cassandra = require(process.argv[2]);
-const { getClientArgs, insertDeSer } = require("./utils");
+const utils = require("./utils");
 const { exit } = require("process");
 
-const client = new cassandra.Client(getClientArgs());
+const client = new cassandra.Client(utils.getClientArgs());
 const iterCount = parseInt(process.argv[3]);
 
 async.series(
     [
-        function connect(next) {
-            client.connect(next);
-        },
-        function createKeyspace(next) {
-            // Keep replication one to reduce time spent in the database
-            const query =
-                "CREATE KEYSPACE IF NOT EXISTS benchmarks WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': '1' }";
-            client.execute(query, next);
-        },
-        // Drop table to ensure no overhead comes from having already some data in the database
-        function dropTable(next) {
-            const query =
-                "DROP TABLE IF EXISTS benchmarks.basic";
-            client.execute(query, next);
-        },
-        function createTable(next) {
-            const query =
-                "CREATE TABLE benchmarks.basic (id uuid, val int, tuuid timeuuid, ip inet, date date, time time, PRIMARY KEY(id))";
-            client.execute(query, next);
+        function initialize(next) {
+            utils.prepareDatabase(client, utils.tableSchemaDeSer, next);
         },
         async function insert(next) {
-            const query =
-                "INSERT INTO benchmarks.basic (id, val, tuuid, ip, date, time) VALUES (?, ?, ?, ?, ?, ?)";
             for (let i = 0; i < iterCount; i++) {
                 try {
-                    await client.execute(query, insertDeSer(cassandra), {prepare: true});
+                    await client.execute(utils.DesSerInsertStatement, utils.insertDeSer(cassandra), { prepare: true });
                 } catch (err) {
                     return next(err);
                 }
@@ -53,7 +34,7 @@ async.series(
             }
             next();
         },
-        function r(){
+        function r() {
             exit(0);
         }
     ],
@@ -62,7 +43,7 @@ async.series(
             console.error("There was an error", err.message, err.stack);
             exit(1);
         }
-        
+
     },
 );
 

benchmark/logic/ser.js
@@ -2,47 +2,28 @@
 const async = require("async");
 // Possible values of argv[2] (driver) are scylladb-javascript-driver and cassandra-driver.
 const cassandra = require(process.argv[2]);
-const { getClientArgs, insertDeSer } = require("./utils");
+const utils = require("./utils");
 const { exit } = require("process");
 
-const client = new cassandra.Client(getClientArgs());
+const client = new cassandra.Client(utils.getClientArgs());
 const iterCount = parseInt(process.argv[3]);
 
 async.series(
     [
-        function connect(next) {
-            client.connect(next);
-        },
-        function createKeyspace(next) {
-            // Keep replication one to reduce time spent in the database
-            const query =
-                "CREATE KEYSPACE IF NOT EXISTS benchmarks WITH replication = {'class': 'NetworkTopologyStrategy', 'replication_factor': '1' }";
-            client.execute(query, next);
-        },
-        // Drop table to ensure no overhead comes from having already some data in the database
-        function dropTable(next) {
-            const query =
-                "DROP TABLE IF EXISTS benchmarks.basic";
-            client.execute(query, next);
-        },
-        function createTable(next) {
-            const query =
-                "CREATE TABLE benchmarks.basic (id uuid, val int, tuuid timeuuid, ip inet, date date, time time, PRIMARY KEY(id))";
-            client.execute(query, next);
+        function initialize(next) {
+            utils.prepareDatabase(client, utils.tableSchemaDeSer, next);
         },
         async function insert(next) {
-            const query =
-                "INSERT INTO benchmarks.basic (id, val, tuuid, ip, date, time) VALUES (?, ?, ?, ?, ?, ?)";
             for (let i = 0; i < iterCount * iterCount; i++) {
                 try {
-                    await client.execute(query, insertDeSer(cassandra), {prepare: true});
+                    await client.execute(utils.DesSerInsertStatement, utils.insertDeSer(cassandra), { prepare: true });
                 } catch (err) {
                     return next(err);
                 }
             }
             next();
         },
-        function r(){
+        function r() {
             exit(0);
         }
     ],
@@ -51,7 +32,7 @@ async.series(
             console.error("There was an error", err.message, err.stack);
             exit(1);
         }
-        
+
     },
 );
 