scylladb-zpp-2024-javascript-driver/scylladb-javascript-driver

Add a new load balancing policy
This is a policy that is a wrapper for the Rust driver default load balancing policy,
The goal for this policy is to hold configuration options,
that will be passed to the Rust, during the session creation.

lib/policies/index.d.ts
@@ -65,6 +65,18 @@ export namespace policies {
         filter?: (host: Host) => boolean;
       });
     }
+    
+    class LoadBalancingConfig {
+      preferDatacenter?: string;
+      preferRack?: string;
+      tokenAware?: boolean;
+      permitDcFailover?: boolean;
+      enableShufflingReplicas?: boolean;
+    }
+    
+    class NewDefaultLoadBalancingPolicy extends LoadBalancingPolicy {
+      constructor(config?: LoadBalancingConfig);
+    }
   }
 
   namespace reconnection {

adespawn/nodejs

lib/policies/index.d.ts
@@ -63,6 +63,18 @@ export namespace policies {
         filter?: (host: Host) => boolean;
       });
     }
+    
+    class LoadBalancingConfig {
+      preferDatacenter?: string;
+      preferRack?: string;
+      tokenAware?: boolean;
+      permitDcFailover?: boolean;
+      enableShufflingReplicas?: boolean;
+    }
+    
+    class DefaultLoadBalancingPolicy extends LoadBalancingPolicy {
+      constructor(config?: LoadBalancingConfig);
+    }
   }
 
   namespace reconnection {